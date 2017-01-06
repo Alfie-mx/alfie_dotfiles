9038c264c21abc590f478bbd70a87b0074789153
Alfie-mx <alfredo.rdz.joya@gmail.com>
2017-01-05 23:47:12 -0800

    for loop for dotfiles working

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@
 
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#EXTRA_DIR="$HOME/.extra"
+# EXTRA_DIR="$HOME/.extra"
 
 echo $DOTFILES_DIR
 
 # Update dotfiles itself first
 
-#[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+# [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 
 # Bunch of symlinks
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
-ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+# ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+# ln -sfv "$DOTFILES_DIR/runcom/.gemrc" ~
+# ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+# ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
